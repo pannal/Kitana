2eac7790b0a2941e3c56511a4e15a1f94bb0f45d
panni <panni@fragstore.net>
2018-11-04 06:15:22 +0100

    0.1.5

--- kitana.py
+++ kitana.py
@@ -58,7 +58,7 @@ def maintenance():
 
 class Kitana(object):
     PRODUCT_IDENTIFIER = "Kitana"
-    VERSION = "0.1.5b2"
+    VERSION = "0.1.5"
     CLIENT_IDENTIFIER_BASE = "{}_{}".format(PRODUCT_IDENTIFIER, VERSION)
     initialized = False
     timeout = 5
