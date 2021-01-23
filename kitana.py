0a818c5a9f70695990d298d736b041774bb468f0
panni <panni@fragstore.net>
2021-01-23 03:59:19 +0100

    bump 0.3.3-1

--- kitana.py
+++ kitana.py
@@ -61,7 +61,7 @@ def maintenance():
 
 class Kitana(object):
     PRODUCT_IDENTIFIER = "Kitana"
-    VERSION = "0.3.3"
+    VERSION = "0.3.3-1"
     CLIENT_IDENTIFIER_BASE = "{}_{}".format(PRODUCT_IDENTIFIER, VERSION)
     initialized = False
     timeout = 5
